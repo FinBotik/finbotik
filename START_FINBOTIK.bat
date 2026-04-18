741d7d5150faffb1a1a6fa22b27ccb90e2af7c2c
FinBotik <borcezdravev@gmail.com>
2026-04-18 18:17:26 +0200

    Create one-click startup script for FinBotik v3.0

---
+++ START_FINBOTIK.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem FinBotik v3.0 one-click startup script
+rem Current Date and Time: 2026-04-18 16:16:40
+rem Current User: FinBotik
+
+start /d "path_to_finbotik_directory" FinBotik.exe
